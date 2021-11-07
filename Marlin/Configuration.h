6762e89256e5c22a9888b3db211dfe917558058f
Jesús Romeo <jrmougan@gmail.com>
2021-11-07 15:07:22 +0100

    pasos extrusor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -896,7 +896,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 134.8 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 141.8 }
 
 /**
  * Default Max Feed Rate (mm/s)
